33800673077d0bacc6928d6a0b849dcb4eafb53b
Steven Whitfield <stevmwhitfield@gmail.com>
2022-11-15 12:13:19 -0500

    Describe main loop and built-in commands

--- steven.c
+++ steven.c
@@ -4,6 +4,17 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+// built-in commands:
+// exit: call exit(0) system call; accepts no args
+// cd: call chdir() system call; accepts exactly 1 arg
+// path: sets the shell's path; accepts 0 or more args
+// loop: loops a command some times; accepts 2 or more args
+
 int main(int argc, char** argv) {
+  // while status is good
+  //   prompt for input
+  //   parse input
+  //   if input is valid, execute command
+  //   else, error message
   return 0;
 }
